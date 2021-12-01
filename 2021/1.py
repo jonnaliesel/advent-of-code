5bb832f6f683402bdbbff8e6047a53ba46b46651
jonnaliesel <jonnaliesel@gmail.com>
2021-12-01 18:52:48 +0100

    add part 2

--- 2021/1.py
+++ 2021/1.py
@@ -4,15 +4,20 @@ increased = 0
 for line in open('1.txt', 'r').readlines():
     data.append(int(line.strip()))
 
-for index, item in enumerate(data) :
+for index, item in enumerate(data):
     if item > data[index -1]:
         increased += 1
 
+print (f'Distance increase: {increased}')
+
 increased = 0
 
-for count, value in enumerate(data) :
-    if value > data[count -1]:
-        increased += 1
+for count, value in enumerate(data):
+    if 0 < count < len(data) - 2:
+        window = value + data[count + 1] + data[count + 2]
+        prev_window = data[count - 1] + value + data[count + 1]
 
-print (f'Distance increase: {increased}')
+        if window > prev_window:
+            increased += 1
+        
 print (f'Window increase: {increased}')
