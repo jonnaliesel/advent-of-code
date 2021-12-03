7d4933742560a8985c8734d676bc079b7533d28d
jonnaliesel <jonnaliesel@gmail.com>
2021-12-03 19:18:55 +0100

    add solution to part 2

--- 2021/2.py
+++ 2021/2.py
@@ -24,3 +24,35 @@ print('*** Part one ***')
 print (f'Horizontal: {horizontal}')
 print (f'Depth: {depth}')
 print (f'Product: {depth * horizontal}\n')
+
+# part two
+
+'''
+down X increases your aim by X units.
+up X decreases your aim by X units.
+forward X does two things:
+It increases your horizontal position by X units.
+It increases your depth by your aim multiplied by X.
+'''
+depth = 0
+horizontal = 0
+aim = 0
+
+for index, item in enumerate(data):
+    position = item.split()
+    direction = position[0]
+    steps = int(position[1])
+
+    if direction == 'up' :
+        aim -= steps
+    elif direction == 'down' :
+        aim += steps
+    elif direction == 'forward' :
+        horizontal += steps
+        depth += aim * steps
+
+print('*** Part two ***')
+print (f'Horizontal: {horizontal}')
+print (f'Depth: {depth}')
+print (f'Aim: {aim}')
+print (f'Product: {depth * horizontal}')
