c64c094474e71416a40841e838e42be8eed80ab0
jonnaliesel <jonnaliesel@gmail.com>
2021-12-05 11:28:18 +0100

    add solution part 2

--- 2021/3.py
+++ 2021/3.py
@@ -1,3 +1,6 @@
+from os import remove
+
+
 data = []
 modified_data = {}
 
@@ -31,4 +34,29 @@ for report in modified_data.values():
 
 print(f'Gamma rate: {int(gamma_rate, 2)}')
 print(f'Epsilon date: {int(epsilon_rate, 2)}')
-print(f'Power consumption: {int(gamma_rate, 2) * int(epsilon_rate, 2)}')
+print(f'Power consumption: {int(gamma_rate, 2) * int(epsilon_rate, 2)}\n')
+
+# part two
+
+oxygen = list(data)
+co2 = list(data)
+
+for i in range(len(data[0])):
+    if len(oxygen) > 1:
+        ox_zeros = len([x  for x in oxygen if x[i]=='0'])
+        ox_ones = len([x  for x in oxygen if x[i]=='1'])
+        if ox_ones >= ox_zeros:
+            oxygen = [x for x in oxygen if x[i]=='1']
+        else:
+            oxygen = [x for x in oxygen if x[i]=='0']
+    if len(co2) > 1:
+        co_zeros = len([x  for x in co2 if x[i]=='0'])
+        co_ones = len([x  for x in co2 if x[i]=='1'])
+        if co_ones >= co_zeros:
+            co2 = [x for x in co2 if x[i]=='0']
+        else:
+            co2 = [x for x in co2 if x[i]=='1']
+
+print(f'Oxygen generator rating: {int(oxygen[0], 2)}')
+print(f'CO2 scrubber rating: {int(co2[0], 2)}')
+print(f'Life support rating: {int(oxygen[0], 2) * int(co2[0], 2)}')
