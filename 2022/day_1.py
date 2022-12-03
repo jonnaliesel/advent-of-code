6ac0f5134b72e0f525c16904570175dca753373d
jonnaliesel <jonnaliesel@gmail.com>
2022-12-03 22:52:52 +0100

    Day one done

---
+++ 2022/day_1.py
@@ -0,0 +1,23 @@
+from puzzle_input import get_puzzle_input
+
+all_elves = get_puzzle_input(1)
+
+
+def get_elf_with_most_calories(all_elves):
+    elves = []
+    elf_with_most = 0
+    calories = 0
+    for snack in all_elves:
+        if not snack == "\n":
+            calories += int(snack)
+        else:
+            elves.append(calories)
+            if calories > elf_with_most:
+                elf_with_most = calories
+            calories = 0
+    elves.sort(reverse=True)
+    print(elves[0:3])
+    return (elf_with_most, (elves[0] + elves[1] + elves[2]))
+
+
+print(get_elf_with_most_calories(all_elves))
