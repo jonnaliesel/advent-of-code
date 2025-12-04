7ab5492863335c0fe8c01980582f6dcb156327f9
Jonna Fleischanderl <jonna.liesel@booli.se>
2025-12-04 22:09:30 +0100

    Add part 2 and break out validation

--- 2025/day_2.py
+++ 2025/day_2.py
@@ -3,7 +3,29 @@ from puzzle_input import get_puzzle_input_string
 id_ranges = get_puzzle_input_string("2").split(",")
 id_ranges_test = get_puzzle_input_string("2_test").split(",")
 
-def find_and_sum_invalid_ids(id_ranges:list[str]) -> int:
+def id_validation(id: int, p2: bool) -> bool:
+    id_str = str(id)
+
+    for k in range(2, len(id_str) + 1 if p2 else 3):
+        if len(id_str) % k != 0:
+            continue
+        
+        segment_size = len(id_str) // k
+        first_segment = id_str[:segment_size]
+        
+        all_match = True
+        for i in range(0, len(id_str), segment_size):
+            if id_str[i:i + segment_size] != first_segment:
+                all_match = False
+                break
+        
+        if all_match:
+            return True
+    
+    return False
+
+
+def find_and_sum_invalid_ids(id_ranges:list[str], p2:bool) -> int:
     sum = 0
     for id_range in id_ranges:
         start, end = id_range.split("-")
@@ -11,19 +33,17 @@ def find_and_sum_invalid_ids(id_ranges:list[str]) -> int:
         end = int(end)
 
         for id in range(start, end+1):
-            if len(str(id)) % 2 != 0:
-                continue
-            else: 
-                seq_length = len(str(id)) // 2
-                if str(id)[:seq_length] == str(id)[seq_length:]:
-                    sum += id
+            if id_validation(id, p2):
+                sum += id
 
     return sum
 
 def test_answer_1():
-    assert find_and_sum_invalid_ids(id_ranges_test) == 1227775554
+    assert find_and_sum_invalid_ids(id_ranges_test, False) == 1227775554
+
+def test_answer_2():
+    assert find_and_sum_invalid_ids(id_ranges_test, True) == 4174379265
 
 if __name__ == "__main__":
-    print(find_and_sum_invalid_ids(id_ranges))
-    # print("Passcode 1: " + str(get_passcode_1(rotation_instructions)))
-    # print("Passcode 2: " + str(get_passcode_2(rotation_instructions)))
+    print(find_and_sum_invalid_ids(id_ranges, False))
+    print(find_and_sum_invalid_ids(id_ranges, True))
